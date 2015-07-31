a36117e8d2aafcddad3f60aac816b1fcb3bc3e0e
waue <waue0920@gmail.com>
2015-07-31 14:39:39 +0800

    sqoop example;

---
+++ sqoop/ex1/exc1.sql
@@ -0,0 +1,9 @@
+DROP DATABASE books;
+CREATE DATABASE IF NOT EXISTS books;
+USE books;
+CREATE TABLE IF NOT EXISTS authors (id INT, name VARCHAR(20), email VARCHAR(20));
+SHOW TABLES;
+INSERT INTO authors (id,name,email) VALUES(1,"Vivek","xuz@abc.com");
+INSERT INTO authors (id,name,email) VALUES(2,"Priya","p@gmail.com");
+INSERT INTO authors (id,name,email) VALUES(3,"Tom","tom@yahoo.com");
+SELECT * FROM authors;
---
+++ sqoop/ex2/update_hdfs_data.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+hadoop fs -rmr sqoop_input
+hadoop fs -put sqoop_input ./
